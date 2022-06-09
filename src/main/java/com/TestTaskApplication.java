8fb6222a984ea0c1742e258e7a38179f31505139
Yaroslav <tangut1998@gmail.com>
2022-06-09 13:52:55 +0400

    added needed dependencies.

---
+++ src/main/java/com/TestTaskApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class TestTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskApplication.class, args);
+    }
+}
